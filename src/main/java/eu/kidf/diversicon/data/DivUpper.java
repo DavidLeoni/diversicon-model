cb7f0c036496cd380c4c4a577277111c3118df17
David Leoni <davidleonigit-github@yahoo.com>
2017-01-02 16:03:25 +0100

    Merged #1 introduce DivUpper for domains
    - Fixed SenseAxis, SenseAxisRelation for https://github.com/diversicon-kb/diversicon-core/issues/15
    - introduced DivUpper lexical resource

---
+++ src/main/java/eu/kidf/diversicon/data/DivUpper.java
@@ -0,0 +1,89 @@
+package eu.kidf.diversicon.data;
+
+import javax.annotation.Nullable;
+
+import eu.kidf.diversicon.core.BuildInfo;
+import eu.kidf.diversicon.core.LexResPackage;
+
+/**
+ * Diversicon Upper Lexical Resource   
+ * 
+ * @since 0.1.0
+ *
+ */
+public class DivUpper extends LexResPackage {
+
+    
+    /**
+     * @since 0.1.0
+     */
+    public static final String SHORT_NAME = "upper";
+    
+    /**
+     * @since 0.1.0
+     */
+    public static final String NAME = "div-" + SHORT_NAME;
+    
+    /**
+     * 
+     * @since 0.1.0
+     */
+    public static final String LABEL = "Diversicon Upper Lexical Resource";    
+    
+    /**
+     * @since 0.1.0
+     */
+    public static final String PREFIX = "div";
+
+    /**
+     * @since 0.1.0
+     */
+    private static final String CLASSPATH = "classpath:/" + NAME;
+    
+    
+    /**
+     * @since 0.1.0
+     */
+    public static final String XML_URI = CLASSPATH + ".xml";    
+    
+    /**
+     * @since 0.1.0
+     */
+    @Nullable
+    private static BuildInfo buildInfo;
+        
+    /**
+     * @since 0.1.0
+     */
+    private static final DivUpper INSTANCE = new DivUpper();   
+    
+    static {
+        INSTANCE.setName(NAME);
+        INSTANCE.setPrefix(PREFIX);
+        INSTANCE.setLabel(LABEL);
+        //INSTANCE.setH2DbUri(CLASSPATH + ".h2.db");
+        //INSTANCE.setSqlUri(CLASSPATH + ".sql");
+        INSTANCE.setXmlUri(CLASSPATH + ".xml");
+        INSTANCE.setSampleXmlUri(CLASSPATH + ".xml"); // sample of itself
+        if (BuildInfo.hasProperties(DivUpper.class)){
+            BuildInfo buildInfo = BuildInfo.of(DivUpper.class);
+            INSTANCE.setVersion(buildInfo.getVersion());
+            INSTANCE.putNamespace(PREFIX, buildInfo.sourceAtTag("master", "src/main/resources/" + SHORT_NAME + ".xml") );
+        } else {            
+            throw new IllegalStateException("Couldn't find properties file " + BuildInfo.BUILD_PROPERTIES_PATH + " for class " + DivUpper.class.getCanonicalName());
+        }        
+        
+    }
+       
+    /**
+     * @since 0.1.0
+     */
+    public static DivUpper of(){
+        return INSTANCE;
+    }
+
+   
+}
+
+
+
